8e6732f10b1bfa14a6f8a9c6df4a33aa80e115d5
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-21 04:23:11 +0500

    @Media

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Navbar from './Components/Navbar/Navbar';
 import FirstSection from './Components/Sections/FirstSection';
 import ClientSection from './Components/Sections/ClientSection';
 import Card1 from './Components/Sections/Card1';
-import SecsondSection from './Components/Sections/SecsondSection';
-import ThirdSection from './Components/Sections/ThirdSection';
-import FourthSection from './Components/Sections/FourthSection';
-import FifhtSection from './Components/Sections/FifhtSection';
+// import SecsondSection from './Components/Sections/SecsondSection';
+// import ThirdSection from './Components/Sections/ThirdSection';
+// import FourthSection from './Components/Sections/FourthSection';
+// import FifhtSection from './Components/Sections/FifhtSection';
+// import Card2 from './Components/Sections/Card2';
+// import Demo from './Components/Sections/Demo';
+// import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -16,10 +19,13 @@ function App() {
       <FirstSection />
       <ClientSection />
       <Card1 />
-      <SecsondSection />
-      <ThirdSection />
-      <FourthSection />
-      <FifhtSection />
+      {/* <SecsondSection /> */}
+      {/* <ThirdSection /> */}
+      {/* <FourthSection /> */}
+      {/* <FifhtSection /> */}
+      {/* <Card2 /> */}
+      {/* <Demo /> */}
+      {/* <Footer />  */}
     </div>
   );
 }
---
+++ src/Components/Sections/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Demo.css'
+
+export default function Demo() {
+    return (
+        <div className='m-o'>
+            <h1>Pellentesque suscipit
+                fringilla libero eu.</h1>
+            <button>Get a Demo &#x2192;</button>
+        </div>
+    )
+}
