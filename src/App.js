188c2213feae373e1a69b3e9e0d0800b9ae6de8d
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-21 17:02:01 +0500

    Underconstruction

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Navbar from './Components/Navbar/Navbar';
 import FirstSection from './Components/Sections/FirstSection';
 import ClientSection from './Components/Sections/ClientSection';
 import Card1 from './Components/Sections/Card1';
-// import SecsondSection from './Components/Sections/SecsondSection';
-// import ThirdSection from './Components/Sections/ThirdSection';
+import SecsondSection from './Components/Sections/SecsondSection';
+import ThirdSection from './Components/Sections/ThirdSection';
 // import FourthSection from './Components/Sections/FourthSection';
 // import FifhtSection from './Components/Sections/FifhtSection';
 // import Card2 from './Components/Sections/Card2';
@@ -16,11 +16,12 @@ function App() {
   return (
     <div >
       <Navbar />
+
       <FirstSection />
       <ClientSection />
       <Card1 />
-      {/* <SecsondSection /> */}
-      {/* <ThirdSection /> */}
+      <SecsondSection />
+      <ThirdSection />
       {/* <FourthSection /> */}
       {/* <FifhtSection /> */}
       {/* <Card2 /> */}
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,23 +1,79 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import logo from '../../Assets/Logo.png';
+// import './Navbar.css';
+
+// const Navbar = () => {
+//     const [menuOpen, setMenuOpen] = useState(false);
+
+
+//     const handleMenuItemClick = () => {
+//         setMenuOpen(false);
+//     };
+
+//     return (
+//         <div>
+
+//             <nav>
+//                 <input type="checkbox" id="click" checked={menuOpen} onChange={() => setMenuOpen(!menuOpen)} />
+//                 <label htmlFor="click" className="menu-btn">
+//                     <i className="fas fa-bars"></i>
+//                     <div><a href='/'>< img className='logoo' src={logo} alt="" /></a></div>
+//                 </label>
+//                 <ul>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>Home</a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>Service</a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>Feature</a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>Product</a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>Testimonial</a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>FAQ</a></li>
+//                     <li > <a onClick={handleMenuItemClick} href='/'><button className='btn btn-login show'>Login</button></a></li>
+//                     <li ><a onClick={handleMenuItemClick} href='/'>  <button className='btn btn-signup show'>SignUp</button></a></li>
+
+//                 </ul>
+//                 <div className='btn-main hide'> <button className='btn btn-login hide'>Login</button>
+//                     <button className='btn btn-signup hide'>SignUp</button></div>
+
+
+//             </nav>
+//         </div>
+//     );
+// };
+
+// export default Navbar;
+
+
+import React, { useState, useEffect } from 'react';
 import logo from '../../Assets/Logo.png';
 import './Navbar.css';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
-
     const handleMenuItemClick = () => {
         setMenuOpen(false);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuOpen && !event.target.closest('nav')) {
+                setMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [menuOpen]);
+
     return (
         <div>
-
-            <nav>
+            <nav >
                 <input type="checkbox" id="click" checked={menuOpen} onChange={() => setMenuOpen(!menuOpen)} />
                 <label htmlFor="click" className="menu-btn">
                     <i className="fas fa-bars"></i>
-                    <div><a href='/'>< img className='logoo' src={logo} alt="" /></a></div>
+                    <div><a href='/'><img className='logoo' src={logo} alt="" /></a></div>
                 </label>
                 <ul>
                     <li ><a onClick={handleMenuItemClick} href='/'>Home</a></li>
@@ -28,13 +84,11 @@ const Navbar = () => {
                     <li ><a onClick={handleMenuItemClick} href='/'>FAQ</a></li>
                     <li > <a onClick={handleMenuItemClick} href='/'><button className='btn btn-login show'>Login</button></a></li>
                     <li ><a onClick={handleMenuItemClick} href='/'>  <button className='btn btn-signup show'>SignUp</button></a></li>
-
                 </ul>
                 <div className='btn-main hide'> <button className='btn btn-login hide'>Login</button>
                     <button className='btn btn-signup hide'>SignUp</button></div>
-
-
             </nav>
+
         </div>
     );
 };
